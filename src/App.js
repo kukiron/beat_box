28d1d23627c331dd659a0855881ee5220835a13d
Kafil Uddin Kiron <kukiron@users.noreply.github.com>
2017-08-02 17:20:47 +0600

    Beat_Box index.html

--- src/App.js
+++ src/App.js
@@ -306,7 +306,7 @@ class App extends Component {
     const {updateBPM, start, stop, addTrack, share, randomSong, closeDialog} = this;
     return (
       <div className="app">
-        <h3>tinysynth</h3>
+        <h3>Beat_Box</h3>
         {shareHash ?
           <ShareDialog hash={shareHash} closeDialog={closeDialog} /> : null}
         <table>
