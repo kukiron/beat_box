2c7709104dd7311c44378d5ddab443f4fcb78bfb
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2017-08-31 04:08:08 +0600

    Add raven configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Raven from 'raven-js';
+
 import App from './App';
+
+const sentry_key = '9d117eedb2724369b5e1e34f0812521d';
+const sentry_app = '210658';
+const sentry_url = `https://${sentry_key}@sentry.io/${sentry_app}`;
+
+Raven.config(sentry_url).install();
+
+Raven.showReportDialog();
 
 ReactDOM.render(
   <App />,
