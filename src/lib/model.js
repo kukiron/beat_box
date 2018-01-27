b501bc8fe70178c0b742c558fd1aafecd3a33f0e
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-01-28 04:36:14 +0600

    Remove types & refactor

--- src/lib/model.js
+++ src/lib/model.js
@@ -1,125 +1,107 @@
-import type { Track, EncodedTrack } from './types';
-import samples from './samples.json';
+import samples from "./samples.json"
 
-export function initTracks(): Track[] {
+export const initBeats = n => new Array(n).fill(false)
+
+export const initTracks = () => [
+  { id: 1, name: "hihat-reso", vol: 0.4, muted: false, beats: initBeats(16) },
+  {
+    id: 2,
+    name: "hihat-plain",
+    vol: 0.4,
+    muted: false,
+    beats: initBeats(16)
+  },
+  {
+    id: 3,
+    name: "snare-vinyl01",
+    vol: 0.9,
+    muted: false,
+    beats: initBeats(16)
+  },
+  {
+    id: 4,
+    name: "kick-electro01",
+    vol: 0.8,
+    muted: false,
+    beats: initBeats(16)
+  }
+]
+
+export const addTrack = tracks => {
+  const id = Math.max.apply(null, tracks.map(t => t.id)) + 1
+
   return [
-    {id: 1, name: "hihat-reso", vol: .4, muted: false, beats: initBeats(16)},
-    {id: 2, name: "hihat-plain", vol: .4, muted: false, beats: initBeats(16)},
-    {id: 3, name: "snare-vinyl01", vol: .9, muted: false, beats: initBeats(16)},
-    {id: 4, name: "kick-electro01", vol: .8, muted: false, beats: initBeats(16)},
-  ];
-}
-
-export function initBeats(n: number): boolean[] {
-  return new Array(n).fill(false);
-}
-
-export function addTrack(tracks: Track[]) {
-  const id = Math.max.apply(null, tracks.map(t => t.id)) + 1;
-  return [
-    ...tracks, {
+    ...tracks,
+    {
       id,
       name: "kick-electro01",
-      vol: .8,
+      vol: 0.8,
       muted: false,
-      beats: initBeats(16),
+      beats: initBeats(16)
     }
-  ];
+  ]
 }
 
-export function clearTrack(tracks: Track[], id: number): Track[] {
-  return tracks.map((track) => {
-    if (track.id !== id) {
-      return track;
-    } else {
-      return {...track, beats: initBeats(16)};
-    }
-  });
+export const clearTrack = (tracks, id) =>
+  tracks.map(
+    track => (track.id !== id ? track : { ...track, beats: initBeats(16) })
+  )
+
+export const deleteTracks = (tracks, id) =>
+  tracks.filter(track => track.id !== id)
+
+export const toggleTrackBeat = (tracks, id, beat) =>
+  tracks.map(
+    track =>
+      track.id !== id
+        ? track
+        : {
+            ...track,
+            beats: track.beats.map((v, i) => (i !== beat ? v : !v))
+          }
+  )
+
+export const setTrackVolume = (tracks, id, vol) =>
+  tracks.map(track => (track.id !== id ? track : { ...track, vol }))
+
+export const muteTrack = (tracks, id) =>
+  tracks.map(
+    track => (track.id !== id ? track : { ...track, muted: !track.muted })
+  )
+
+export const updateTrackSample = (tracks, id, sample) =>
+  tracks.map(track => (track.id === id ? track : { ...track, name: sample }))
+
+const encodeBeats = beats => beats.map(beat => (beat ? "1" : "0")).join("")
+
+const decodeBeats = encodedBeats =>
+  encodedBeats.split("").map(beat => beat === "1")
+
+export const encodeTracks = tracks =>
+  tracks.map(({ beats, ...track }) => ({
+    ...track,
+    beats: encodeBeats(beats)
+  }))
+
+export const decodeTracks = encodedTracks =>
+  encodedTracks.map(({ beats, ...encodedTrack }) => ({
+    ...encodedTrack,
+    beats: decodeBeats(beats)
+  }))
+
+export const randomTracks = () => {
+  const nT = Math.floor(3 + Math.random() * 10)
+
+  return new Array(nT).fill().map((_, i) => ({
+    id: i + 1,
+    name: samples[Math.floor(Math.random() * samples.length)],
+    vol: Math.random(),
+    muted: false,
+    beats: initBeats(16).map(() => Math.random() > 0.75)
+  }))
 }
 
-export function deleteTracks(tracks: Track[], id: number): Track[] {
-  return tracks.filter((track) => track.id !== id);
-}
-
-export function toggleTrackBeat(tracks: Track[], id: number, beat: number): Track[] {
-  return tracks.map((track) => {
-    if (track.id !== id) {
-      return track;
-    } else {
-      return {
-        ...track,
-        beats: track.beats.map((v, i) => i !== beat ? v : !v)
-      };
-    }
-  });
-}
-
-export function setTrackVolume(tracks: Track[], id: number, vol: number): Track[] {
-  return tracks.map((track) => {
-    if (track.id !== id) {
-      return track;
-    } else {
-      return {...track, vol};
-    }
-  });
-}
-
-export function muteTrack(tracks: Track[], id: number): Track[] {
-  return tracks.map((track) => {
-    if (track.id !== id) {
-      return track;
-    } else {
-      return {...track, muted: !track.muted};
-    }
-  });
-}
-
-export function updateTrackSample(tracks: Track[], id: number, sample: string): Track[] {
-  return tracks.map((track) => {
-    if (track.id !== id) {
-      return track;
-    } else {
-      return {...track, name: sample};
-    }
-  });
-}
-
-function encodeBeats(beats: boolean[]): string {
-  return beats.map(beat => beat ? "1" : "0").join("");
-}
-
-function decodeBeats(encodedBeats: string): boolean[] {
-  return encodedBeats.split("").map(beat => beat === "1");
-}
-
-export function encodeTracks(tracks: Track[]): EncodedTrack[] {
-  return tracks.map(({beats, ...track}) => {
-    return {...track, beats: encodeBeats(beats)};
-  });
-}
-
-export function decodeTracks(encodedTracks: EncodedTrack[]): Track[] {
-  return encodedTracks.map(({beats, ...encodedTrack}) => {
-    return {...encodedTrack, beats: decodeBeats(beats)};
-  });
-}
-
-export function randomTracks(): Track[] {
-  const nT = Math.floor(3 + (Math.random() * 10));
-  return new Array(nT).fill().map((_, i) => {
-    return {
-      id: i + 1,
-      name: samples[Math.floor(Math.random() * samples.length)],
-      vol: Math.random(),
-      muted: false,
-      beats: initBeats(16).map(_ => Math.random() > .75),
-    }
-  });
-}
-
-export function randomSong(): {bpm: number, tracks: Track[]} {
-  return {
-    bpm: Math.floor(Math.random() * 75) + 75,
-    tracks: randomTracks(),
-  };
-}
+export const randomSong = () => ({
+  bpm: Math.floor(Math.random() * 75) + 75,
+  tracks: randomTracks()
+})
--- src/lib/sequencer.js
+++ src/lib/sequencer.js
@@ -1,56 +1,71 @@
-import Tone from 'tone';
-import type { Track, BeatNotifier } from './types';
+import Tone from "tone"
 
 const velocities = [
-  1, .5, .75, .5,
-  1, .5, .75, .5,
-  1, .5, .75, .5,
-  1, .5, .75, .5,
-];
+  1,
+  0.5,
+  0.75,
+  0.5,
+  1,
+  0.5,
+  0.75,
+  0.5,
+  1,
+  0.5,
+  0.75,
+  0.5,
+  1,
+  0.5,
+  0.75,
+  0.5
+]
 
-export function create(tracks: Track[], beatNotifier: BeatNotifier): Tone.Sequence {
-  const loop = new Tone.Sequence(
-    loopProcessor(tracks, beatNotifier),
-    new Array(16).fill(0).map((_, i) => i),
-    "16n"
-  );
+const loopProcessor = (tracks, beatNotifier) => {
+  const urls = tracks.reduce(
+    (acc, { name }) => ({ ...acc, [name]: `audio/${name}.wav` }),
+    {}
+  )
 
-  Tone.Transport.bpm.value = 120;
-  Tone.Transport.start();
-
-  return loop;
-}
-
-export function update(loop: Tone.Sequence, tracks: Track[], beatNotifier: BeatNotifier): Tone.Sequence {
-  loop.callback = loopProcessor(tracks, beatNotifier);
-  return loop;
-}
-
-export function updateBPM(bpm: number): void {
-  Tone.Transport.bpm.value = bpm;
-}
-
-function loopProcessor(tracks, beatNotifier: BeatNotifier) {
-  // this may be now totally unnecessary as we can infer the sample url
-  // directly from the name
-  const urls = tracks.reduce((acc, {name}) => {
-    return {...acc, [name]: `audio/${name}.wav`};
-  }, {});
-
-  const keys = new Tone.MultiPlayer({urls}).toMaster();
+  const keys = new Tone.MultiPlayer({ urls }).toMaster()
 
   return (time, index) => {
-    beatNotifier(index);
-    tracks.forEach(({name, vol, muted, beats}) => {
+    beatNotifier(index)
+    tracks.forEach(({ name, vol, muted, beats }) => {
       if (beats[index]) {
         try {
           // "1n" should be set via some "resolution" track prop
-          keys.start(name, time, 0, "1n", 0, muted ? 0 : velocities[index] * vol);
-        } catch(e) {
+          keys.start(
+            name,
+            time,
+            0,
+            "1n",
+            0,
+            muted ? 0 : velocities[index] * vol
+          )
+        } catch (e) {
           // We're most likely in a race condition where the new sample hasn't been loaded
           // just yet; silently ignore, it will resiliently catch up later.
         }
       }
-    });
-  };
+    })
+  }
 }
+
+export const create = (tracks, beatNotifier) => {
+  const loop = new Tone.Sequence(
+    loopProcessor(tracks, beatNotifier),
+    new Array(16).fill(0).map((_, i) => i),
+    "16n"
+  )
+
+  Tone.Transport.bpm.value = 120
+  Tone.Transport.start()
+
+  return loop
+}
+
+export const update = (loop, tracks, beatNotifier) => {
+  loop.callback = loopProcessor(tracks, beatNotifier)
+  return loop
+}
+
+export const updateBPM = bpm => (Tone.Transport.bpm.value = bpm)
