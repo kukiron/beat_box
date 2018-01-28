22522b3fee641e01f895b953387573a6f4bed221
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-01-28 16:30:00 +0600

    Add express server for heroku deployment

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express"),
+  path = require("path"),
+  port = process.env.PORT || 8080,
+  app = express()
+
+app.use(express.static(__dirname))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"))
+})
+
+app.listen(port, () => console.log(`Server started on ${port}`))
