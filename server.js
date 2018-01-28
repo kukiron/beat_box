b4c389459322add27bcdabd7b2b2ba563ee57bf8
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-01-28 22:23:17 +0600

    Deploy gh-pages build

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const express = require("express"),
   app = express()
 
 // Setup static assets
-app.use("/build", express.static(path.resolve(__dirname, "./build")))
+app.use(
+  "/build/bundle.js",
+  express.static(path.resolve(__dirname, "./build/bundle.js"))
+)
 app.use(
   "/public/favicon.ico",
   express.static(path.resolve(__dirname, "./public/favicon.ico"))
